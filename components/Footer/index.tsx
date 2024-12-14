7e589df4367c81ee39ec11abb076521dbb2b4816
kavorix <kavorix5@gmail.com>
2024-12-15 04:10:09 +0900

    fix error

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,5 @@
 
 import { BsGithub } from "react-icons/bs"
-import { FaLinkedinIn } from "react-icons/fa";
 import { CustomLink } from "../CustomLink"
 
 export const Footer = () => {
@@ -8,9 +7,6 @@ export const Footer = () => {
     <div className="flex flex-col gap-2 p-4">
       <div className='flex items-center justify-center gap-6'>
         <hr className='w-10 bg-white' />
-        <CustomLink href="https://www.linkedin.com/in/sato-daiki-japan/">
-          <FaLinkedinIn color='white' />
-        </CustomLink>
         <CustomLink href="https://github.com/kavorix">
           <BsGithub color='white' />
         </CustomLink>
